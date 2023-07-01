49bf16165bd1be35c1df0f35037cdcde3b7dab5a
Acesif <asifpalak@gmail.com>
2023-07-01 19:32:16 +0600

    adding C/F unit toggler

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import OtherStats from './Component/OtherStats';
 import DayForecast from './Component/DayForecast';
 
 export const context = createContext()
-
+export const unit = createContext()
 function App() {
   const [data,setData] = useState()
+  const [unit,setUnit] = useState(true)
   return (
     <div className="App">
-      <context.Provider value={[data,setData]}>
-        <Search/>
-        <Current/>
-        <OtherStats/>
-        <DayForecast/>
-      </context.Provider>
+      <unitContext.Provider value = {[unit,setUnit]}>
+        <context.Provider value={[data,setData]}>
+         <Search/>
+         <Current/>
+         <OtherStats/>
+         <DayForecast/>
+        </context.Provider>
+      <unitContext.Provider>
     </div>
   );
 }
